a140ac1254ff2259eecd17fc535a004e94dc4d3d
Kieran Osgood <kieranbosgood@gmail.com>
2022-12-03 22:31:24 +0000

    feat(day-2): part2 complete - horrible functional...

--- src/day-2/index.ts
+++ src/day-2/index.ts
@@ -5,6 +5,9 @@ import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray'
 import {data} from "./data";
 import {add} from "../utils/number";
 
+// const data = `A Y
+// B X
+// C Z`
 
 // MY ENEMY
 type Enemy = 'A' | 'B' | 'C'
@@ -44,34 +47,88 @@ const roundPoints = {
     lose: 0,
     draw: 3,
     win: 6,
-}
+} as const
 
-const calculatePoints = (a: RNEA.ReadonlyNonEmptyArray<string>): number => {
-    const [enemy, my] = a as [Enemy, Me]
+const getResultPart1 = (my: Me, enemy: Enemy): 'win' | 'draw' | 'lose' => {
     const [enemyMove, myMove] = [decodeMove[enemy], decodeMove[my]]
-
     const winMove = winResponse[enemy]
+
     const winResult = my === winMove
     const drawResult = enemyMove === myMove
     const loseResult = !winResult && !drawResult
-    const movePointResult = movePoints[my]
 
     switch (true) {
         case winResult:
-            return roundPoints.win + movePointResult
+            return 'win'
         case drawResult:
-            return roundPoints.draw + movePointResult
+            return 'draw'
         case loseResult:
-            return roundPoints.lose + movePointResult
+            return 'lose'
     }
 }
 
-const res = pipe(
+const calculatePointsPart1 = (a: RNEA.ReadonlyNonEmptyArray<string>): number => {
+    const [enemy, my] = a as [Enemy, Me]
+    const result = getResultPart1(my, enemy)
+    const movePointResult = movePoints[my]
+
+    return movePointResult + roundPoints[result]
+}
+
+const part1Result = pipe(
     data,
     S.split('\n'),
     RNEA.map(S.split(' ')),
-    RNEA.map(calculatePoints),
+    RNEA.map(calculatePointsPart1),
     RNEA.reduce(0, add)
 )
 
-res // ?
+part1Result // ?
+
+
+const resultWantedMap = {
+    'X': 'lose',
+    'Y': 'draw',
+    'Z': 'win',
+} as const
+
+const movePointsMap = {
+    rock: 1,
+    paper: 2,
+    scissors: 3,
+} as const
+
+const loseResponse = {
+    [enemyRock]: scissors,
+    [enemyPaper]: rock,
+    [enemyScissors]: paper,
+} as const
+
+const getMyMove = (enemy: Enemy, resultWanted: typeof resultWantedMap[keyof typeof resultWantedMap]) => {
+    switch (resultWanted) {
+        case 'draw':
+            return enemy
+        case 'win':
+            return winResponse[enemy]
+        case 'lose':
+            return loseResponse[enemy]
+    }
+}
+
+const calculatePointsPart2 = (a: RNEA.ReadonlyNonEmptyArray<string>): number => {
+    const [enemy, resultWantedInstruction] = a as [Enemy, Me]
+    const resultWanted = resultWantedMap[resultWantedInstruction]
+    const myMove = getMyMove(enemy, resultWanted)
+    const movePointResult = movePointsMap[decodeMove[myMove]]
+    return movePointResult + roundPoints[resultWanted]
+}
+
+const part2Result = pipe(
+    data,
+    S.split('\n'),
+    RNEA.map(S.split(' ')),
+    RNEA.map(calculatePointsPart2),
+    RNEA.reduce(0, add)
+)
+
+part2Result // ?
