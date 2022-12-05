9c500393e6246d46ead15d8917912bc5f0353fd3
Kieran Osgood <kieranbosgood@gmail.com>
2022-12-05 17:34:15 +0000

    feat(day-1): part1 complete

--- src/day-3/index.ts
+++ src/day-3/index.ts
@@ -1,120 +1,77 @@
-import * as S from 'fp-ts/lib/string'
-import * as SStd from 'fp-ts-std/string'
-import * as AStd from 'fp-ts-std/array'
-import * as NES from 'fp-ts-std/NonEmptyString'
-import * as A from 'fp-ts/Array'
-import * as Predicate from 'fp-ts/Predicate'
-import * as O from 'fp-ts/Option'
-import * as E from 'fp-ts/Either'
-import * as B from 'fp-ts/Boolean'
-import {flow, pipe, tupled,} from 'fp-ts/function'
-import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray'
-// import {data} from "./data";
-import {add} from "../utils/number";
-import {identity} from "io-ts";
-import * as RA from "fp-ts/ReadonlyArray";
-import {curry2,} from "fp-ts-std/Function";
+import * as S from "fp-ts/lib/string"
+import * as SStd from "fp-ts-std/string"
+import * as AStd from "fp-ts-std/array"
+import * as A from "fp-ts/Array"
+import * as O from "fp-ts/Option"
+import * as B from "fp-ts/Boolean"
+import { flow, pipe } from "fp-ts/function"
+import * as RA from "fp-ts/ReadonlyArray"
+import { curry2 } from "fp-ts-std/Function"
+import * as assert from "assert"
+import { data } from "./data"
 
-// const data = `vJrwpWtwJgWrhcsFMMfFFhFp
-// jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
-// PmmdzqPrVvPwwTWBwg
-// wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
-// ttgJtRGJQctTZtZT
-// CrZsJsPPZsGzwwsLwLmpwMDw`
-
-const addUpperValue = (char: string) => {
-    const upper = SStd.isUpper(char)
-
-    return pipe(
-        char,
-        SStd.isUpper,
-        B.match(
-            () => 0,
-            () => 26
-        )
-    )
-}
-
-const getCharValue = (char: string) => (s: string) => pipe(
-    char,
-    NES.unsafeFromString,
-    NES.toLowerCase,
-    NES.unNonEmptyString
-) === s
-import * as TE from "fp-ts/TaskEither";
-
-const getIndex = (char: string) => {
-    const alphabet = ['_', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    return pipe(
-        alphabet,
-        A.findIndex(curry2(S.Eq.equals)(char)),
-        // O.getOrElseW(() => 'not found')
-        O.getOrElseW(() => -1)
-    )
-
+const getCharIndex = (char: string) => {
+  // prettier-ignore
+  const alphabet = ['_', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  return pipe(
+    alphabet,
+    A.findIndex(curry2(S.Eq.equals)(char)),
+    O.getOrElseW(() => -1),
+  )
 }
 
 const calculateCharValue = (char: string) => {
-    return pipe(
-        char,
-        O.of,
-        O.map(c => pipe(
-            c,
-            SStd.isUpper,
-            B.fold(
-                () => getIndex(c),
-                () => getIndex(S.toLowerCase(c)) + 26,
-            )
-        )),
-        O.getOrElseW(() => 0)
-    )
+  return pipe(
+    O.of(char),
+    O.map((c) =>
+      pipe(
+        c,
+        SStd.isUpper,
+        B.fold(
+          () => getCharIndex(c),
+          () => getCharIndex(S.toLowerCase(c)) + 26,
+        ),
+      ),
+    ),
+    O.getOrElseW(() => 0),
+  )
 } // ?
 
-// calculateCharValue('a') // ?
-// calculateCharValue('b') // ?
-// calculateCharValue('A') // ?
-// calculateCharValue('B') // ?
-// calculateCharValue('p') // ?
-// calculateCharValue('asd') // ?
+const splitStringInTwo = (id: string): [string, string] => SStd.splitAt(id.length / 2)(id)
 
-// expect 16
+const splitByChars = flow(A.map(S.split("")), A.map(RA.toArray))
 
-const data = `vJrwpWtwJgWrhcsFMMfFFhFp
+const splitByLines = flow(S.split("\n"), RA.toArray)
+
+const tupleIntersection = (t: string[][]) => A.intersection(S.Eq)(t[0])(t[1])
+
+const intersectionList = flow(
+  splitByLines,
+  A.chain(
+    flow(
+      splitStringInTwo, //
+      splitByChars,
+      A.map(A.uniq(S.Eq)),
+      tupleIntersection,
+    ),
+  ),
+  (id) => id,
+  A.map(calculateCharValue),
+  AStd.sum,
+)
+
+const testData = `vJrwpWtwJgWrhcsFMMfFFhFp
 jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
 PmmdzqPrVvPwwTWBwg
 wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
 ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw`
-// expect 157 total
 
-const splitStringInTwo = (id: string): [string, string] => SStd.splitAt(id.length / 2)(id)
+const testDataTotal = intersectionList(testData) // ?
+assert.strictEqual(testDataTotal, 157) // ?
 
-const splitByChars = flow(
-    A.map(S.split('')),
-    A.map(RA.toArray)
-)
+const part1Result = intersectionList(data) // ?
+assert.strictEqual(part1Result, 8085) // ?
 
-const splitByLines = flow(
-    S.split('\n'),
-    RA.toArray
-)
-
-const tupleIntersection = (a: string[][]) => A.intersection(S.Eq)(a[0])(a[1])
-
-const intersectionList = pipe(
-    data,
-    splitByLines,
-    A.chain(
-        flow(
-            splitStringInTwo,
-            splitByChars,
-            tupleIntersection,
-        )
-    ),
-    A.uniq(S.Eq),
-    A.map(calculateCharValue),
-    AStd.sum
-)
-
-intersectionList // ?
-// expect 157 total
+const part2Result = intersectionList(data) // ?
+assert.strictEqual(part1Result, 8085) // ?
