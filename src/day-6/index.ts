2d34e0871401a785ab15724ce06a8a199ec29ef5
Kieran Osgood <kieranbosgood@gmail.com>
2022-12-08 20:44:35 +0000

    feat(day-6): setup file structure

---
+++ src/day-6/index.ts
@@ -0,0 +1,12 @@
+import * as S from "fp-ts/string"
+import * as A from "fp-ts/Array"
+import * as O from "fp-ts/Option"
+import * as E from "fp-ts/Either"
+import { flow } from "fp-ts/function"
+import * as RA from "fp-ts/ReadonlyArray"
+import * as assert from "assert"
+import { NumberFromString } from "io-ts-types"
+import { Validation } from "io-ts"
+import { data } from "./data"
+
+const testData = ``
