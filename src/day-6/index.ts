88f1304d2bbe1fb1557b9ac2d185780b76260891
Kieran Osgood <kieranbosgood@gmail.com>
2022-12-10 11:27:00 +0000

    feat(day-6): part1+part2 complete

--- src/day-6/index.ts
+++ src/day-6/index.ts
@@ -9,4 +9,37 @@ import { NumberFromString } from "io-ts-types"
 import { Validation } from "io-ts"
 import { data } from "./data"
 
-const testData = ``
+export function sliding(
+  window: number,
+  offset: number,
+): <A>(xs: ReadonlyArray<A>) => ReadonlyArray<ReadonlyArray<A>> {
+  return (xs) =>
+    xs.length < window ? [] : [xs.slice(0, window), ...sliding(window, offset)(xs.slice(offset))]
+}
+
+const testData = [
+  `mjqjpqmgbljsphdztnvjfqwrcgsmlb`,
+  `bvwbjplbgvbhsrlpgdmjqwftvncz`,
+  "nppdvjthqldpwncqszvftbrmjlhg",
+  "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",
+  "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw",
+]
+
+const part1 = (window: number) =>
+  flow(
+    S.split(""), //
+    sliding(window, 1),
+    RA.map(RA.uniq(S.Eq)),
+    RA.findIndex((a) => a.length === window),
+    O.map((a) => a + window),
+    O.getOrElse(() => -1),
+  )
+
+assert.strictEqual(part1(4)(testData[0]), 7)
+assert.strictEqual(part1(4)(testData[1]), 5)
+assert.strictEqual(part1(4)(testData[2]), 6)
+assert.strictEqual(part1(4)(testData[3]), 10)
+assert.strictEqual(part1(4)(testData[4]), 11)
+
+assert.strictEqual(part1(4)(data), 1343)
+assert.strictEqual(part1(14)(data), 2193)
